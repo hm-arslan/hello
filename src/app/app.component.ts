46bbf52ef93553c399ee79252abae1e2dbf346f8
hm-arslan <arslan.91.maqsood@hotmail.com>
2024-06-03 14:37:52 +0500

    Added changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import {Component} from '@angular/core';
   standalone: true,
   imports: [],
   template: `
-    <h1>Default</h1>
+    <h1>Default1</h1>
   `,
   styleUrls: ['./app.component.css'],
 })
